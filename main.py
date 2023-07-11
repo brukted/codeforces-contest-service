a967e34f01d9e16b6985883b04073b534c8071e0
Bruk Tedla <biruk.tedla93@gmail.com>
2023-07-11 22:40:39 +0300

    improve docs

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-import json
 import os
-from fastapi import FastAPI, HTTPException, Request
-import httpcore
+
 import httpx
-from models.domain.problem import Problem
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 
-
+from models.domain.problem import Problem
+from models.domain.standing import Standing
+from models.domain.submission import Submission
 from models.requests.contest_summary_request import ContestSummaryRequest
 from models.responses.contest_summary import ContestSummary
 from models.responses.jsend_response import JSendResponse
 from scraper.page_loader import PageLoader
-
-from dotenv import load_dotenv
-
 from service.codeforces_service import CodeForcesService
 
 load_dotenv()
+
+description = """
+The CodeForces Contest API is a service that provides 
+access to various features and information related to CodeForces contests. 
+It allows users to retrieve contest summaries, submissions, problems, and standings for a specific contest.
+"""
+
+app = FastAPI(title="CodeForces Contest API", version="0.1.0", description=description)
 
 configuration = {
     "handleOrEmail": os.getenv("CODEFORCES_HANDLE"),
     "password": os.getenv("CODEFORCES_PASSWORD"),
 }
-
-app = FastAPI()
 
 
 @app.exception_handler(httpx.ReadTimeout)
@@ -34,8 +38,9 @@ async def unicorn_exception_handler(request: Request, exc: httpx.ReadTimeout):
     )
 
 
-@app.get("/")
+@app.get("/health-check")
 def health_check() -> JSendResponse[None]:
+    """Performs a health check for the API. Returns a JSendResponse indicating the status of the API."""
     return JSendResponse(message="OK", data=None)
 
 
@@ -43,6 +48,10 @@ def health_check() -> JSendResponse[None]:
 async def get_contest_summary(
     gym_id: int, request: ContestSummaryRequest
 ) -> JSendResponse[ContestSummary]:
+    """Retrieves a summary of a CodeForces contest with the specified gym ID.
+
+    Note: Some contestants may be discarded based on virtual participation and deadline.
+    """
     request.gym_id = gym_id
     page_loader = PageLoader(configuration)
     await page_loader.authenticate()
@@ -52,16 +61,8 @@ async def get_contest_summary(
     )
 
 
-@app.get("/contest/{gym_id}")
-async def get_contest(gym_id: int):
-    raise HTTPException(
-        status_code=400,
-        detail="Not implemented",
-    )
-
-
 @app.get("/contest/{gym_id}/submissions")
-async def get_contest_submissions(gym_id: int):
+async def get_contest_submissions(gym_id: int) -> JSendResponse[list[Submission]]:
     page_loader = PageLoader(configuration)
     await page_loader.authenticate()
     codeforces_service = CodeForcesService(page_loader)
@@ -81,7 +82,7 @@ async def get_contest_problems(gym_id: int) -> JSendResponse[list[Problem]]:
 
 
 @app.get("/contest/{gym_id}/standings")
-async def get_contest_standings(gym_id: int):
+async def get_contest_standings(gym_id: int) -> JSendResponse[list[Standing]]:
     page_loader = PageLoader(configuration)
     await page_loader.authenticate()
     codeforces_service = CodeForcesService(page_loader)
